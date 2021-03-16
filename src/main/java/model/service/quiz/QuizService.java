3cd46e3a9d61edc60203ceb4cab6b3235024c89d
DuyTran1811 <ferssken99@gmail.com>
2021-03-16 21:30:48 +0700

    test

--- src/main/java/model/service/quiz/QuizService.java
+++ src/main/java/model/service/quiz/QuizService.java
@@ -23,7 +23,7 @@ public class QuizService implements IQuiz {
                 int id_user = rs.getInt(2);
                 String name = rs.getString(3);
                 String difficulty = rs.getString(4);
-                list.add(new Quiz(id, id_user, name, difficulty));
+//                list.add(new Quiz(id, id_user, name, difficulty));
             }
         } catch (SQLException e) {
             e.printStackTrace();
