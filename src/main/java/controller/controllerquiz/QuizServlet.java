a75b518f8b9bfd59a6825ab301b8c4bf30c5ed47
DuyTran1811 <ferssken99@gmail.com>
2021-03-20 22:53:40 +0700

    fix

--- src/main/java/controller/controllerquiz/QuizServlet.java
+++ src/main/java/controller/controllerquiz/QuizServlet.java
@@ -109,7 +109,6 @@ public class QuizServlet extends HttpServlet {
         request.setAttribute("lisqq", list);
         RequestDispatcher dispatcher = request.getRequestDispatcher("managerquiz/create.jsp");
         dispatcher.forward(request, response);
-//
     }
 
     private void Trigger(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
